fd2bb3e4c1393f11c1fc8cb628cd7e7eac3c47e9
Isika Powers <isika.powers@gmail.com>
2021-07-12 15:27:53 -0600

    Create ship spec

---
+++ spec/ship_spec.rb
@@ -0,0 +1,52 @@
+require "RSpec"
+require "./lib/ship"
+
+RSpec.describe Ship do
+  it "exists" do
+    ship = Ship.new("Cruiser", 3)
+
+    expect(ship).to be_an_instance_of(Ship)
+  end
+
+  it "has a name" do
+    ship = Ship.new("Cruiser", 3)
+
+    expect(ship.name).to eq("Cruiser")
+  end
+
+  it "has a length" do
+    ship = Ship.new("Cruiser", 3)
+
+    expect(ship.length).to eq(3)
+  end
+
+  it "starts with health equal to length" do
+    ship = Ship.new("Cruiser", 3)
+
+    expect(ship.health).to eq(3)
+  end
+
+  it "sinks" do
+    ship = Ship.new("Cruiser", 3)
+
+    expect(ship.sunk?).to eq(false)
+
+    ship.hit
+    ship.hit
+
+    expect(ship.sunk?).to eq(false)
+
+    ship.hit  
+
+    expect(ship.sunk?).to eq(true)
+
+  end
+
+  it "hits" do
+    ship = Ship.new("Cruiser", 3)
+    ship.hit
+
+    expect(ship.health).to eq(2)
+  end
+
+end
