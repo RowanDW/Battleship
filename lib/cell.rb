5cd63695a5269b9ef17a5516393b09d4d5706a5d
Isika Powers <isika.powers@gmail.com>
2021-07-12 16:06:55 -0600

    Create Cell class

---
+++ lib/cell.rb
@@ -0,0 +1,28 @@
+class Cell
+
+  attr_reader :coordinate, :ship, :fired_upon
+
+  def initialize(coordinate)
+    @coordinate = coordinate
+    @ship = nil
+    @fired_upon = false
+  end
+
+  def empty?
+    @ship == nil
+  end
+
+  def place_ship(ship_name)
+    @ship = ship_name
+  end
+
+  def fired_upon?
+    @fired_upon
+  end
+
+  def fire_upon
+    @ship.hit
+    @fired_upon = true
+  end
+
+end
